ab9dcc3e495a4218e63de164cd1ac300d87807ab
Divyam19 <divyamgupta19@gmail.com>
2024-10-13 23:45:29 +0530

    init sender logs

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -8,20 +8,23 @@ pragma solidity ^0.8.18;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe{
-    uint256 public minUsd = 5;
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded ;
+    address[] public funders;
+    uint256 public minUsd = 5e18;
     function fund() public payable {
         //Allow users to send $
         //Have a minimum $ sent
         //How do we send ETh to this contract
 
-        require(msg.value > minUsd , "didnt send enough eth"); // 1e18 = 1ETH = 1000000000000000000 = 1 * 10**18
-
+        require(getConversionRate(msg.value) >= minUsd , "didnt send enough eth"); // 1e18 = 1ETH = 1000000000000000000 = 1 * 10**18
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
 
     }
     
     
     //function withdraw() public {}
-    function getprice() public view returns (uint256){ 
+    function getPrice() public view returns (uint256){ 
         //Address
         //ABI
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
@@ -34,7 +37,11 @@ contract FundMe{
         //2000.00000000
         return uint256(price * 1e18);
     }
-    function getConversionRate() public {}
+    function getConversionRate(uint256 ethAmount) public view returns (uint256){
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18 ;
+        return ethAmountInUsd;
+    }
 
 }
 
