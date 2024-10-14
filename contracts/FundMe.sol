0e7ab5aaafbdc7b30852385c2ed34e71ce2264c4
Divyam19 <divyamgupta19@gmail.com>
2024-10-14 22:45:15 +0530

    init PriceConverter library

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -4,19 +4,14 @@
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
-
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
-
+import {PriceConverter} from "contracts/PriceConverter.sol";
 contract FundMe{
+    using PriceConverter for uint256;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded ;
     address[] public funders;
     uint256 public minUsd = 5e18;
     function fund() public payable {
-        //Allow users to send $
-        //Have a minimum $ sent
-        //How do we send ETh to this contract
-
-        require(getConversionRate(msg.value) >= minUsd , "didnt send enough eth"); // 1e18 = 1ETH = 1000000000000000000 = 1 * 10**18
+        require(msg.value.getConversionRate() >= minUsd , "didnt send enough eth"); // 1e18 = 1ETH = 1000000000000000000 = 1 * 10**18
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
 
@@ -24,24 +19,7 @@ contract FundMe{
     
     
     //function withdraw() public {}
-    function getPrice() public view returns (uint256){ 
-        //Address
-        //ABI
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (/*uint80 roundId*/,
-        int price,
-        /*uint startedAt*/,
-        /*uint timeStamp*/,
-        /*uint80 answeredInRound*/ ) = priceFeed.latestRoundData();
-        // pricein terms of usd
-        //2000.00000000
-        return uint256(price * 1e18);
-    }
-    function getConversionRate(uint256 ethAmount) public view returns (uint256){
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18 ;
-        return ethAmountInUsd;
-    }
+    
 
 }
 
---
+++ contracts/PriceConverter.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter{
+    function getPrice() internal view returns (uint256){ 
+        //Address
+        //ABI
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (/*uint80 roundId*/,
+        int price,
+        /*uint startedAt*/,
+        /*uint timeStamp*/,
+        /*uint80 answeredInRound*/ ) = priceFeed.latestRoundData();
+        // pricein terms of usd
+        //2000.00000000
+        return uint256(price * 1e18);
+    }
+
+    function getConversionRate(uint256 ethAmount) internal view returns (uint256){
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18 ;
+        return ethAmountInUsd;
+    }
+
+    function getVersion() internal view returns (uint256){
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    }
+}
